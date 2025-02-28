c8b455387f92a3d356f099be72a1ee4d9c032682
hamadismail <hamad.ismail.gub@gmail.com>
2025-03-01 00:33:55 +0600

    implement-activity-log

--- scripts/script.js
+++ scripts/script.js
@@ -31,6 +31,8 @@ const btnPrimary = document.querySelectorAll('.btn-primary');
 
 for (const btn of btnPrimary) {
   btn.addEventListener('click', e => {
+    alert('Board Updated Successfully');
+
     btn.disabled = true;
     btn.classList.add('opacity-20');
 
@@ -45,5 +47,37 @@ for (const btn of btnPrimary) {
     const itemvalue = parseInt(item.innerText);
 
     item.innerText = Math.max(0, itemvalue + 1);
+
+    // add activity log
+    const activityContainer = document.getElementById('activity-container');
+    const activityTitle =
+      btn.parentElement.parentElement.querySelector('h2').innerText;
+
+    const p = document.createElement('p');
+    p.classList.add(
+      'rounded-lg',
+      'text-sm',
+      'p-2',
+      'text-gray-800',
+      'bg-[var(--clr-bg)]'
+    );
+
+    // get current time
+    const now = new Date();
+
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    let ampm = hours >= 12 ? 'PM' : 'AM';
+
+    hours = hours % 12 || 12;
+
+    minutes = String(minutes).padStart(2, '0');
+    seconds = String(seconds).padStart(2, '0');
+
+    const timeString = `${hours}:${minutes}:${seconds} ${ampm}`;
+
+    p.innerText = `You have completed the tasks for ${activityTitle} at ${timeString}`;
+    activityContainer.appendChild(p);
   });
 }
